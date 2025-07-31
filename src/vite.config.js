bc7c0a04c55441e6eaee6bc2a08e4ed3ad769d7d
Dvp005 <sg.devivaraprasad@gmail.com>
2025-07-31 22:41:22 +0530

    Update vite.config.js

--- src/vite.config.js
+++ src/vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/dvp-portfolio-/', // Only repo name here (must match GitHub repo name exactly)
+  base: 'dvp-portfolio-', // Only repo name here (must match GitHub repo name exactly)
   plugins: [react()],
 })
